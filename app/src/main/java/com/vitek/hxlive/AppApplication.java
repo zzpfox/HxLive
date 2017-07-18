bee324d7fbd7680975422c299c640c9cdfec5bca
javakam <123222557@qq.com>
2017-07-18 14:16:27 +0800

    Initial commit

---
+++ app/src/main/java/com/vitek/hxlive/AppApplication.java
@@ -0,0 +1,104 @@
+package com.vitek.hxlive;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.IntentFilter;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+import com.vmloft.develop.library.tools.VMApplication;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by lzan13 on 2016/5/25.
+ *
+ * 程序入口，做一些必要的初始化操作
+ */
+public class AppApplication extends VMApplication {
+
+    private CallReceiver callReceiver;
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        // 初始化环信sdk
+        initHyphenate();
+    }
+
+    /**
+     * 初始化环信sdk，并做一些注册监听的操作
+     */
+    private void initHyphenate() {
+        // 获取当前进程 id 并取得进程名
+        int pid = android.os.Process.myPid();
+        String processAppName = getAppName(pid);
+        /**
+         * 如果app启用了远程的service，此application:onCreate会被调用2次
+         * 为了防止环信SDK被初始化2次，加此判断会保证SDK被初始化1次
+         * 默认的app会在以包名为默认的process name下运行，如果查到的process name不是app的process name就立即返回
+         */
+        if (processAppName == null || !processAppName.equalsIgnoreCase(context.getPackageName())) {
+            // 则此application的onCreate 是被service 调用的，直接返回
+            return;
+        }
+
+        // 初始化sdk的一些配置
+        EMOptions options = new EMOptions();
+        options.setAutoLogin(true);
+        // 动态设置appkey，如果清单配置文件设置了 appkey，这里可以不用设置
+        //options.setAppKey("easemob-demo#chatdemoui");
+
+        // 设置小米推送 appID 和 appKey
+        options.setMipushConfig("2882303761517573806", "5981757315806");
+
+        // 设置消息是否按照服务器时间排序
+        options.setSortMessageByServerTime(false);
+
+        // 初始化环信SDK,一定要先调用init()
+        EMClient.getInstance().init(context, options);
+
+        // 开启 debug 模式
+        EMClient.getInstance().setDebugMode(true);
+
+        // 设置通话广播监听器
+        IntentFilter callFilter = new IntentFilter(EMClient.getInstance().callManager().getIncomingCallBroadcastAction());
+        if (callReceiver == null) {
+            callReceiver = new CallReceiver();
+        }
+        //注册通话广播接收者
+        context.registerReceiver(callReceiver, callFilter);
+
+        // 通话管理类的初始化
+        CallManager.getInstance().init(context);
+    }
+
+    /**
+     * 根据Pid获取当前进程的名字，一般就是当前app的包名
+     *
+     * @param pid 进程的id
+     * @return 返回进程的名字
+     */
+    private String getAppName(int pid) {
+        String processName = null;
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List list = activityManager.getRunningAppProcesses();
+        Iterator i = list.iterator();
+        while (i.hasNext()) {
+            ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo) (i.next());
+            try {
+                if (info.pid == pid) {
+                    // 根据进程的信息获取当前进程的名字
+                    processName = info.processName;
+                    // 返回当前进程名
+                    return processName;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        // 没有匹配的项，返回为null
+        return processName;
+    }
+}
